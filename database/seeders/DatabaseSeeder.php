faf2eac69e100a4d3bd3aece9fed9f0249f42087
strayneko <bayurendi10@gmail.com>
2023-02-14 21:19:10 +0700

    :sparkle: add post and category factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Category;
+use App\Models\Post;
 use App\Models\PostDb;
 use App\Models\User;
 
@@ -21,49 +22,54 @@ class DatabaseSeeder extends Seeder
         Category::truncate();
         PostDb::truncate();
 
-        // user factory
-        $user =   User::factory()->create();
+        // seeding post with factory
+        PostDb::factory(5)->create();
+
+        // manual factory
+        //     // user factory
+        //     $user =   User::factory()->create();
 
 
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
-        ]);
+        //     $personal = Category::create([
+        //         'name' => 'Personal',
+        //         'slug' => 'personal'
+        //     ]);
 
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
+        //     $work = Category::create([
+        //         'name' => 'Work',
+        //         'slug' => 'work'
+        //     ]);
 
-        $hobbies = Category::create([
-            'name' => 'Hobbies',
-            'slug' => 'hobbies'
-        ]);
+        //     $hobbies = Category::create([
+        //         'name' => 'Hobbies',
+        //         'slug' => 'hobbies'
+        //     ]);
 
 
-        PostDb::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My First Post',
-            'slug' => 'my-first-post',
-            'excerpt' => 'lorem aklsdkl;asdkjajdfkljaskldaosfshbdfasf',
-            'body' => '<p>asddddddddddddasd asfksdjfklajsdlkasdj jkashfkasdjaskldasd</p>'
-        ]);
-        PostDb::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My second Post',
-            'slug' => 'my-second-post',
-            'excerpt' => 'lorem aklsdkl;asdkjajdfkljaskldaosfshbdfasf',
-            'body' => '<p>asddddddddddddasd asfksdjfklajsdlkasdj jkashfkasdjaskldasd</p>'
-        ]);
-        PostDb::create([
-            'user_id' => $user->id,
-            'category_id' => $hobbies->id,
-            'title' => 'My third Post',
-            'slug' => 'my-third-post',
-            'excerpt' => 'lorem aklsdkl;asdkjajdfkljaskldaosfshbdfasf',
-            'body' => '<p>asddddddddddddasd asfksdjfklajsdlkasdj jkashfkasdjaskldasd</p>'
-        ]);
+        //     PostDb::create([
+        //         'user_id' => $user->id,
+        //         'category_id' => $personal->id,
+        //         'title' => 'My First Post',
+        //         'slug' => 'my-first-post',
+        //         'excerpt' => 'lorem aklsdkl;asdkjajdfkljaskldaosfshbdfasf',
+        //         'body' => '<p>asddddddddddddasd asfksdjfklajsdlkasdj jkashfkasdjaskldasd</p>'
+        //     ]);
+        //     PostDb::create([
+        //         'user_id' => $user->id,
+        //         'category_id' => $work->id,
+        //         'title' => 'My second Post',
+        //         'slug' => 'my-second-post',
+        //         'excerpt' => 'lorem aklsdkl;asdkjajdfkljaskldaosfshbdfasf',
+        //         'body' => '<p>asddddddddddddasd asfksdjfklajsdlkasdj jkashfkasdjaskldasd</p>'
+        //     ]);
+        //     PostDb::create([
+        //         'user_id' => $user->id,
+        //         'category_id' => $hobbies->id,
+        //         'title' => 'My third Post',
+        //         'slug' => 'my-third-post',
+        //         'excerpt' => 'lorem aklsdkl;asdkjajdfkljaskldaosfshbdfasf',
+        //         'body' => '<p>asddddddddddddasd asfksdjfklajsdlkasdj jkashfkasdjaskldasd</p>'
+        //     ]);
+        // }
     }
 }
