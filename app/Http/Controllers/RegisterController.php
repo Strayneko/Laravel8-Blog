9398ac3179729626e35842fa348b9f4cca22b60e
strayneko <bayurendi10@gmail.com>
2023-02-15 20:37:58 +0700

    :sparkle: Failed Validation and Old Input Data

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -17,8 +17,8 @@ class RegisterController extends Controller
         // validate input
         $attributes = request()->validate([
             'name' => 'required|max:255',
-            'username' => 'required|max:255|min:3',
-            'email' => 'required|max:255|email',
+            'username' => 'required|max:255|min:3|unique:users,username',
+            'email' => 'required|max:255|email|unique:users,email',
             'password' => 'required|min:7|max:255'
         ]);
         // store validated data into database
--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -14,7 +14,10 @@
                     </label>
 
                     <input class="border border-gray-400 p-2 w-full" type="text" name="name" id="name"
-                        required>
+                        value="{{ old('name') }}" required>
+                    @error('name')
+                        <p class="text-xs text-red-500 mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -23,7 +26,11 @@
                     </label>
 
                     <input class="border border-gray-400 p-2 w-full" type="text" name="username" id="username"
-                        required>
+                        value="{{ old('username') }}" required>
+
+                    @error('username')
+                        <p class="text-xs text-red-500 mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -32,7 +39,11 @@
                     </label>
 
                     <input class="border border-gray-400 p-2 w-full" type="email" name="email" id="email"
-                        required>
+                        value="{{ old('email') }}" required>
+
+                    @error('email')
+                        <p class="text-xs text-red-500 mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -44,6 +55,9 @@
                     <input class="border border-gray-400 p-2 w-full" type="password" name="password" id="password"
                         required>
 
+                    @error('password')
+                        <p class="text-xs text-red-500 mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
